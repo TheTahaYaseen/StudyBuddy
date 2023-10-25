d1055f00ad33049035fe12b1d444cef23eb18a95
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-26 03:24:09 +0500

    Registration!

--- base/views.py
+++ base/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.forms import UserCreationForm
+
 from django.db.models import Q
 
 from .models import Room, Topic, User
@@ -16,11 +18,13 @@ from .forms import RoomForm
 
 def login_view(request):
 
+    page = "login"
+
     if request.user.is_authenticated:
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -36,12 +40,30 @@ def login_view(request):
         else:
             messages.error(request, "Username Or Password Is Incorrect!")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+def register_view(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()         
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An Error Occured During Registration!")   
+
+    context = {"form": form}
+    return render(request, "base/login_register.html", context)
+
 
 def home(request):
 
