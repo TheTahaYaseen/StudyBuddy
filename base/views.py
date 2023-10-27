aa7bc48868c2986d179a7c086e542c40bd75a2cf
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-28 02:04:46 +0500

    Implemented Room  Form Template

--- base/views.py
+++ base/views.py
@@ -120,21 +120,36 @@ def user_profile(request, primary_key):
 def create_room(request):
     form = RoomForm()
     
+    topics = Topic.objects.all()
+
     if request.method == "POST":
         form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {"form": form, "isUpdate": False}
+
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get("name"),
+            description = request.POST.get("description"),
+        )
+
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user
+        #     room.save()
+
+        return redirect('home')
+
+    context = {"form": form, "isUpdate": False, "topics": topics}
     return render(request, "base/room_form.html", context)
 
 @login_required(login_url="/login")
 def update_room(request, primary_key):
     
     room = Room.objects.get(id = primary_key)
+    topics = Topic.objects.all()
     
     if request.user != room.host:
         return HttpResponse("Only The Creator / Host Can Update The Room")
@@ -143,11 +158,14 @@ def update_room(request, primary_key):
 
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {"form": form, "isUpdate": True}
+        room.topic = topic,
+        room.name = request.POST.get("name"),
+        room.description = request.POST.get("description"),
+
+    context = {"form": form, "isUpdate": True, "topics": topics, "room": room}
 
     return render(request, "base/room_form.html", context)
 
