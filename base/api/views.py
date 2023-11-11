c1c91487b4e2aa7e76e3bc4f1078cc65e05f0acd
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-12 00:50:34 +0500

    Implemented Serialized Data Return For Rooms In API

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 @api_view(["GET"])
 def get_routes(request):
@@ -9,3 +11,9 @@ def get_routes(request):
         "GET /api/rooms/:id",
     ]
     return Response(routes)
+
+@api_view(["GET"])
+def get_rooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
